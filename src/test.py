97ea2711f14507ea4407ac8671d3a00352875cc7
Matt Venn <matt@mattvenn.net>
2023-06-20 11:18:25 +0200

    tt04 verilog demo

---
+++ src/test.py
@@ -0,0 +1,26 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+
+
+segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
+
+@cocotb.test()
+async def test_7seg(dut):
+    dut._log.info("start")
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
+
+    dut._log.info("reset")
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+
+    dut._log.info("check all segments")
+    for i in range(10):
+        dut._log.info("check segment {}".format(i))
+        await ClockCycles(dut.clk, 1000)
+        assert int(dut.segments.value) == segments[i]
+
+        # all bidirectionals are set to output
+        assert dut.uio_oe == 0xFF
---
+++ src/tt_um_seven_segment_seconds.v
@@ -0,0 +1,55 @@
+`default_nettype none
+
+module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 10_000_000 ) (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
+
+    wire reset = ! rst_n;
+    wire [6:0] led_out;
+    assign uo_out[6:0] = led_out;
+
+    // use bidrectionals as outputs
+    assign uio_oe = 8'b11111111;
+
+    // put bottom 8 bits of second counter out on the bidirectional gpio
+    assign uio_out = second_counter[7:0];
+
+    // external clock is 10MHz, so need 24 bit counter
+    reg [23:0] second_counter;
+    reg [3:0] digit;
+
+    always @(posedge clk) begin
+        // if reset, set counter to 0
+        if (reset) begin
+            second_counter <= 0;
+            digit <= 0;
+        end else begin
+            // if up to 16e6
+            if (second_counter == MAX_COUNT) begin
+                // reset
+                second_counter <= 0;
+
+                // increment digit
+                digit <= digit + 1'b1;
+
+                // only count from 0 to 9
+                if (digit == 9)
+                    digit <= 0;
+
+            end else
+                // increment counter
+                second_counter <= second_counter + 1'b1;
+        end
+    end
+
+    // instantiate segment display
+    seg7 seg7(.counter(digit), .segments(led_out));
+
+endmodule
