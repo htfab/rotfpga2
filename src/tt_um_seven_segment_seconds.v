9d7487325c68575bb1c0ef1c82ad5d178c4f6da9
matt venn <matt@mattvenn.net>
2023-06-28 09:00:24 +0200

    Update tt_um_seven_segment_seconds.v
    fix typo in comment

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -15,7 +15,7 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 10_000_000 ) (
     wire [6:0] led_out;
     assign uo_out[6:0] = led_out;
 
-    // use bidrectionals as outputs
+    // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
 
     // put bottom 8 bits of second counter out on the bidirectional gpio
