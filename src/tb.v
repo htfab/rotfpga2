fc29b7e27b63768887164c7eedb86160a894f860
Matt Venn <matt@mattvenn.net>
2023-07-20 17:22:18 +0200

    updates for GL netlist. Will only pass if the design is hardened with MAX_COUNT set to 1000

--- src/tb.v
+++ src/tb.v
@@ -28,11 +28,13 @@ module tb ();
     wire ena;
 
     // instantiate the DUT with lower MAX_COUNT for a faster sim
+    `ifdef GL_TEST
+    tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
+        .VPWR( 1'b1),
+        .VGND( 1'b0),
+    `else
     tt_um_seven_segment_seconds #(.MAX_COUNT(1000)) tt_um_seven_segment_seconds (
-        `ifdef GL_TEST
-            .vccd1( 1'b1),
-            .vssd1( 1'b0),
-        `endif
+    `endif
         .ui_in      (ui_in),    // Dedicated inputs
         .uo_out     (uo_out),   // Dedicated outputs
         .uio_in     (uio_in),   // IOs: Input path
